ef5691fcb5c0ff2f8ac3390ada8a5a8b07912d54
Alejopool <gt429504@gmail.com>
2024-03-13 10:01:05 -0500

    Agrego cambios a los archivos teniendo en cuenta que se hace la configuracion del proyecto donde se conectan todos los steps con los runners y los feactures para este caso el given

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -5,8 +5,8 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources", 
-                 glue = "src/test/java/steps", 
+@CucumberOptions(features = "src/test/resources", // Directorio de nuestros archivos .feature
+                 glue = "steps", // Paquete donde tenemos nuestras clases definiendo los steps
                  plugin = { "pretty", "html:target/cucumber-reports" })
 
 
